b9f1ec99d796bbec18313f6707a1c491bf9a8752
corbo369 <samdecoursey@gmail.com>
2025-05-10 20:57:41 -0700

    update wagmi project id

--- portfolio/src/configs/wagmiConfig.ts
+++ portfolio/src/configs/wagmiConfig.ts
@@ -8,8 +8,8 @@ import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { mainnet } from "wagmi/chains";
 
 export const config = getDefaultConfig({
-    appName: 'My RainbowKit App',
-    projectId: 'YOUR_PROJECT_ID',
+    appName: 'corfolio',
+    projectId: '2223b57bf6bce08513c61b3eb06018f0',
     chains: [mainnet],
     ssr: true,
 });
